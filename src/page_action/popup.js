d19d595b0ae8796f6fc66971f9b72ff707b374f3
Daniel Nussbaum <daniel.e.nussbaum@gmail.com>
2017-11-12 16:34:45 -0600

    updated extension

--- src/page_action/popup.js
+++ src/page_action/popup.js
@@ -1,3 +1,26 @@
 var payload = chrome.extension.getBackgroundPage().payload;
 
-console.log(payload);
+var app = angular.module("mainpopup", []);
+
+app.controller("popupCtrl", function($scope, $http) {
+
+    $scope.the_url = "";
+    $scope.payload = {};
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      $scope.$apply(function(){
+        $scope.the_url = tabs[0].url;
+
+        // JSON Request to get articles and other data
+        $http.get("/js/json_output.json").then(function(response) {
+          $scope.payload = response.data;
+        });
+      });
+    });
+
+    // Formats the date as needed
+    $scope.format_date = function(date) {
+      var d = new Date(date);
+      return d.toLocaleDateString("en-US");
+    };
+});
